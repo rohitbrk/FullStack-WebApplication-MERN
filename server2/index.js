f3cda5dd36f79b09a7a9328b941368d9edf504f1
Rohith Kumar <89332475+RohithR-kumar@users.noreply.github.com>
2021-12-01 23:45:10 +0530

    Add files via upload
    server

---
+++ server2/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+
+const mongoose = require("mongoose");
+const User = require("./models/user");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/mern");
+
+app.post("/api/register", async (req, res) => {
+  try {
+    await User.create({
+      name: req.body.name,
+      mail: req.body.mail,
+      password: req.body.password,
+      quote: req.body.quote,
+    });
+    res.json({ status: "ok" });
+  } catch (err) {
+    console.log(err);
+    res.json({ status: "error" });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      mail: req.body.mail,
+      password: req.body.password,
+    });
+    res.json({ status: "ok" });
+
+    console.log(user.name);
+    console.log(user.quote);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(5000, () => console.log("server running on PORT 5000"));
